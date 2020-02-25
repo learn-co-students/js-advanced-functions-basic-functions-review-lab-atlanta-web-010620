28c9d8d1fa64cc69b3dba5ed37a845bd2bd6bc06
tylergreason <tyler.greason@protonmail.com>
2020-02-25 16:37:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(para = "special"){
+        return `You are ${flair}${para}${flair}!`
+    }
+}
+
+let Calculator = {
+    add:function(){return 1+3},
+    subtract:function(){return 1-3},
+    multiply:function(){return 1*3},
+    divide:function(){return 10/5}
+}
+
+function actionApplyer(startInt, array){
+    if (array.length > 0){
+        array.forEach(functionInArray => startInt = functionInArray(startInt))
+    }
+    return startInt;
+}
